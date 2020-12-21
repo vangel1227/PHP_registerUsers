436e1267dd09ac77f4a65ce162b0543269db4bd6
Vince <71363740+vangel1227@users.noreply.github.com>
2020-12-20 22:24:09 -0600

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -10,8 +10,7 @@ include('includes/header.html');
 </p>
 
 <p>
-    Projects included here are a trip calculator with a variety of forms that will either retain or delete the information once submitted illustrating the importance of how you write your code and the attributes of your HTML forms. Then 
-    incorporating MySQL and PHP to create a user databse where you view, update and delete user information. There is also a change password feature as long as you know the username and password. Other features include sorting information obtained
+    Projects included here incorporating MySQL and PHP to create a user databse where you can view, update and delete user information. There is also a change password feature as long as you know the username and password. Other features include sorting information obtained
     from a MySQL database and presenting it in a table as well as paginating and formating the tables.
 </p>
 
